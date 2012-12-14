3570eb5b112caedb9c8a6993e06d0578d6d2b143
Xavier Paterne <xavier@remanence-studio.com>
2012-12-14 01:15:29 +0100

    actualisation des templates

--- application/views/new-client.php
+++ application/views/new-client.php
@@ -23,59 +23,59 @@ $part = explode('/', $_GET['a']);
 ?>
 <div class="row">
     <form id="new_client" action="<?php echo Controller::redirect($action, true); ?>" method="post" enctype="multipart/form-data">
-    <? if ($part[2] == 'myprofile'): ?>
-        <div>
-            <label><?php echo $lang["lang_logo"]?></label>
-            <input type="file" name="logo" id="logo" class="wide" />
+        <div class="eight columns centered">
+            <? if ($part[2] == 'myprofile'): ?>
+                <div>
+                    <label><?php echo $lang["lang_logo"]?></label>
+                    <input type="file" name="logo" id="logo" class="wide" />
+                </div>
+            <?php endif; ?>   
+                <div>
+                    <label><?php echo $lang["lang_name"]?></label>
+                    <input type="text" name="name" id="name" class="wide" value="<?php echo $name; ?>"/>
+                </div>
+            
+            <?php if (!$new_admin): ?>
+                <div>
+                    <label><?php echo $lang["lang_contact_person"]?></label>
+                    <input type="text" name="contact_person" id="contact_person" class="wide"
+                           value="<?php echo $contact_person; ?>"/>
+                </div>
+            <?php endif; ?>
+            
+                <div>
+                    <label>Email</label>
+                    <input type="text" name="contact_email" id="contact_email" class="wide"
+                           value="<?php echo $contact_email; ?>"/>
+                </div>
+            
+            <?php if (!$new_admin): ?>
+                <div>
+                    <label><?php echo $lang["lang_phone_number"]?></label>
+                    <input type="text" name="contact_phone" id="contact_phone" class="wide"
+                           value="<?php echo $contact_phone; ?>"/>
+                </div>
+            <?php endif; ?>
+            
+            <?php if ($is_edit): ?>
+                <div>
+                    <label><?php echo $lang["lang_adress"]?></label>
+                    <input type="text" name="address_line_1" id="address_line_1" class="wide"
+                           value="<?php echo $address_line_1; ?>"/>
+                </div>
+                <div>
+                    <label><?php echo $lang["lang_adress_line_two"]?></label>
+                    <input type="text" name="address_line_2" id="address_line_2" class="wide"
+                           value="<?php echo $address_line_2; ?>"/>
+                </div>
+            <?php endif; ?>
+            
+            <?php if ($is_edit): ?>
+                <input type="hidden" id="is_edited" name="is_edited" value="true"/>
+            <?php endif; ?>
+            
+                <div class="clearfix button-container"><input class="button" type="submit" value="<?php echo $lang["lang_submit"]?>">
+                </div>
         </div>
-    <?php endif; ?>   
-        <div>
-            <label><?php echo $lang["lang_name"]?></label>
-            <input type="text" name="name" id="name" class="wide" value="<?php echo $name; ?>"/>
-        </div>
-    
-    <?php if (!$new_admin): ?>
-        <div>
-            <label><?php echo $lang["lang_contact_person"]?></label>
-            <input type="text" name="contact_person" id="contact_person" class="wide"
-                   value="<?php echo $contact_person; ?>"/>
-        </div>
-    <?php endif; ?>
-    
-        <div>
-            <label>Email</label>
-            <input type="text" name="contact_email" id="contact_email" class="wide"
-                   value="<?php echo $contact_email; ?>"/>
-        </div>
-    
-    <?php if (!$new_admin): ?>
-        <div>
-            <label><?php echo $lang["lang_phone_number"]?></label>
-            <input type="text" name="contact_phone" id="contact_phone" class="wide"
-                   value="<?php echo $contact_phone; ?>"/>
-        </div>
-    <?php endif; ?>
-    
-    <?php if ($is_edit): ?>
-        <div>
-            <label><?php echo $lang["lang_adress"]?></label>
-            <input type="text" name="address_line_1" id="address_line_1" class="wide"
-                   value="<?php echo $address_line_1; ?>"/>
-        </div>
-        <div>
-            <label><?php echo $lang["lang_adress_line_two"]?></label>
-            <input type="text" name="address_line_2" id="address_line_2" class="wide"
-                   value="<?php echo $address_line_2; ?>"/>
-        </div>
-    <?php endif; ?>
-    
-    <?php if ($is_edit): ?>
-        <input type="hidden" id="is_edited" name="is_edited" value="true"/>
-    <?php endif; ?>
-    
-        <div class="clearfix button-container"><input class="button" type="submit" value="<?php echo $lang["lang_submit"]?>">
-        </div>
-    
-    
     </form>
 </div>
